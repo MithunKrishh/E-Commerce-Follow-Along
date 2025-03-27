c133df8a1e6483f0dc586c2dfdcb4a60a4dcc6b8
Mithun Krishna A <mithunkannan06@gmail.com>
2025-03-27 13:42:30 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,26 @@
 
 import './App.css';
 import React from 'react';
-import {CreateProduct, Home, Login,Signup} from "./Routes/Routes"
+import {Login,Signup,Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress, MyOrdersPage, OrderConfirmation} from "./Routes/Routes"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/signup' element={<Signup/>}/>
     <Route path='/' element={<Home/>}/>
-    <Route path='/createProduct' element={<CreateProduct/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/create-user' element={<Signup/>}/>
+    <Route path='/create-product' element={<CreateProduct/>}/>  
+    {/* For edit product by id */}
+    <Route path="/create-product/:id" element={<CreateProduct />} />
+    <Route path="/my-products" element={<MyProducts/>} />
+    <Route path="/cart" element={<Cart/>} />
+    <Route path="/product/:id" element={<ProductDetails />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path='/create-address' element={<CreateAddress />} />
+    <Route path='/select-address' element={<SelectAddress/>}/>
+    <Route path="/order-confirmation" element={<OrderConfirmation />} />
+    <Route path="/myorders" element={<MyOrdersPage/>}/>
     </Routes>
     </BrowserRouter>
   );
